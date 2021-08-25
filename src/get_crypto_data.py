41b8595aa03c5836a1baed460d5cdaccd07ef96a
John Funk <funkjo@my.easternct.edu>
2021-08-25 00:06:09 -0400

    collect data for multiple assets, send alerts if price change > 5

--- src/get_crypto_data.py
+++ src/get_crypto_data.py
@@ -21,23 +21,32 @@ def generate_query(assets: list):
                 query = query + asset
             else:
                 query = query + asset + ','
-    query = query + '&data_points=24&time_series_indicators=open,close,high,low'
-
+        
+        query = query + '&data_points=24&time_series_indicators=open,close,high,low'
+    # print(query)
     return query
-
-
-def convert_to_df(time_series):
-    return None
 
 
 def get_crypto_data(assets: list):
     query = generate_query(assets)
     api_call_str = api_assets_uri + query
     r = requests.get(api_call_str)
-
     data = json.loads(r.text)
-    pct_chang_24h = data['data'][0]['percent_change_24h']
-    pct_change_7d = data['data'][0]['percent_change_7d']
-    time_series = data['data'][0]['timeSeries']
-    df = convert_to_df(time_series)
-    return None
+
+    return_dict = {
+
+    }
+
+    for asset in data['data']:
+        time_series = asset['timeSeries']
+        now = time_series[-1]['close']
+        one_hr_ago = time_series[-2]['close']
+        pct_change_1h = (now - one_hr_ago) / now * 100
+        return_dict[asset['symbol']] = {
+            'price': asset['price'],
+            'pct_change_24h': asset['percent_change_24h'],
+            'pct_change_7d': asset['percent_change_7d'],
+            'pct_change_1h': pct_change_1h,
+            'open': one_hr_ago
+        }
+    return return_dict
--- src/main.py
+++ src/main.py
@@ -5,12 +5,23 @@ import time
 import schedule
 import threading
 from get_crypto_data import get_crypto_data
+from emailer import Emailer
 
-assets = ['ADA']
+assets = ['ADA', 'BTC']
+
+
+def send_alert(symbol, pct_change, _type, _open, close):
+    emailer = Emailer()
+    emailer.send_email(symbol, _open, close, pct_change, _type)
+
 
 def get_time_series():
     data = get_crypto_data(assets)
-    # print(data)
+    for key in data:
+        if abs(data[key]['pct_change_1h']) > 5:
+            send_alert(key, data[key]['pct_change_1h'], 'hourly', data[key]['open'], data[key]['price'])
+    print(data)
+
 
 def run_threaded(job_func):
     job_thread = threading.Thread(target=job_func)
